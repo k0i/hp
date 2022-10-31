dd00df186a4a2eea6036d7b7f8e43c19d90985cc
соджиро кояма <koyama10032009@outlook.jp>
2022-10-31 11:04:12 +0300

    :sparkles: add google slide to whoami page

--- front/theme.ts
+++ front/theme.ts
@@ -11,6 +11,7 @@ export const theme = extendTheme({
         lineHeight: "200%",
         bgColor: "black",
         color: "gray.200",
+        fontSize: "md",
       },
       a: {
         color: "teal.500",
